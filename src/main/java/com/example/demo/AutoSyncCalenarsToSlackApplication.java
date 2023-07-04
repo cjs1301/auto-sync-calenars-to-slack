c2f80fc0199a6680421cca6732b32dafb0bbdec7
jaesong <cjs5241@gmail.com>
2023-07-04 18:19:11 +0900

    init

---
+++ src/main/java/com/example/demo/AutoSyncCalenarsToSlackApplication.java
@@ -0,0 +1,13 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AutoSyncCalenarsToSlackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AutoSyncCalenarsToSlackApplication.class, args);
+	}
+
+}
