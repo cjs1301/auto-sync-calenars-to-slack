f592f30910bdb0610144413e25dca213be26a0b6
jaesong <cjs5241@gmail.com>
2023-07-04 19:17:46 +0900

    오타 수정

--- src/main/java/com/example/demo/AutoSyncCalendarsToSlackApplication.java
+++ src/main/java/com/example/demo/AutoSyncCalendarsToSlackApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class AutoSyncCalenarsToSlackApplication {
+public class AutoSyncCalendarsToSlackApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(AutoSyncCalenarsToSlackApplication.class, args);
+		SpringApplication.run(AutoSyncCalendarsToSlackApplication.class, args);
 	}
 
 }
